0dbf6d3cb9e8bd8953efa80bf6ab05bf2df0a16b
Johnnie Hård <johnnie@synvinkel.org>
2022-10-09 21:25:39 +0200

    tailwind

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,12 +1,5 @@
+<script lang="ts">
+	import "../app.css";
+  </script>
+
 <slot />
-
-<style>
-	:global(img) {
-		display: block;
-	}
-
-	:global(html, body) {
-		margin: 0;
-		padding: 0;
-	}
-</style>
